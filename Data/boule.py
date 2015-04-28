38efc151cde7e160db0a6ef780bbe96b9508e14d
CharlieHebdo <kau4114@gmail.com>
2015-04-29 01:58:16 +0200

    zzzerdfsefesfsvdfbfgd

--- Data/boule.py
+++ Data/boule.py
@@ -16,70 +16,57 @@ screen = pygame.display.set_mode(size)
 ###################################################
 class boule_de_feu(pygame.sprite.Sprite):
 
-    def __init__(self,joueur):
+    def __init__(self,bouliste,explosiste,boum):
 
         super().__init__()
-        self.boum = pygame.mixer.Sound("art/explosion.wav")
-        self.pouet = pygame.mixer.Sound("art/pouet.wav")
-
-        self.actif = False
-        self.joueur = joueur
-        
-        if self.joueur.boule_de_feu >= 1:
-            self.image = pygame.image.load("art/boule1.png")
-            self.rect = self.image.get_rect()
-            self.rect.y = -200
-            self.niveau = None
-            self.spriteCount = 0
-            self.impact = False
-            self.pos = pygame.mouse.get_pos()
-            self.joueur.boule_de_feu -= 1
-            self.actif = True
-            self.son = False
-            
-        elif joueur.sounds:
-            self.pouet.play()
+        self.bouliste = bouliste
+        self.explosiste = explosiste
+        self.boum = boum
+        self.image = self.bouliste[0]
+        self.rect = self.image.get_rect()
+        self.rect.y = -200
+        self.niveau = None
+        self.spriteCount = 0
+        self.impact = False
+        self.pos = pygame.mouse.get_pos()
+        self.son = False
 
         
     def update(self):
-        if self.actif == True :
+        if self.rect.y > 220 and not self.son:
+            self.boum.play()
+            self.son = True
+        #detection collision sol                
+        if self.rect.y > 370 and not self.impact:
 
-            if self.rect.y > 220 and self.joueur.sounds and not self.son:
-                self.boum.play()
-                self.son = True
-            #detection collision sol                
-            if self.rect.y > 370 and not self.impact:
+            self.spriteCounti = 0
+            self.impact = True
+            self.image = self.explosiste[0]
+            self.rect = pygame.Rect(self.pos[0]-125,320,190,230)
 
-                self.spriteCounti = 0
-                self.impact = True
-                self.image = pygame.image.load("art/explosion1.png").convert()
-                self.rect = pygame.Rect(self.pos[0]-125,320,190,230)
-
-                for ninja in self.niveau.enemy_list:
-                    if pygame.sprite.collide_rect(self,ninja) == True:
-                        ninja.tuer()
+            for ninja in self.niveau.enemy_list:
+                if pygame.sprite.collide_rect(self,ninja) == True:
+                    ninja.tuer()
 
 
-            if self.impact:
-                
-                spriteN = "art/explosion%s.png"%(int(self.spriteCounti/4+1))
-                self.image = pygame.image.load(spriteN)
-                self.spriteCounti += 1                                
-                if self.spriteCounti == 24:
-                    self.kill()
+        if self.impact:
 
-                            
-            else:
-                # deplacement position/boule
-                self.rect.y = self.rect.y + 15
-                self.rect.x = self.pos[0]-50
-                
-                #animation
-                if (pygame.time.get_ticks())%4 == 0:
-                    spriteN = "art/boule%s.png"%(int(self.spriteCount+1))
-                    self.image = pygame.image.load(spriteN)
-                    self.spriteCount += 1
-                    if self.spriteCount == 6:
-                        self.spriteCount =0
+            self.image = self.explosiste[int(self.spriteCounti/4)]
+            self.spriteCounti += 1                                
+            if self.spriteCounti == 24:
+                self.kill()
+
+                        
+        else:
+            # deplacement position/boule
+            self.rect.y = self.rect.y + 15
+            self.rect.x = self.pos[0]-50
+            
+            #animation
+            if (pygame.time.get_ticks())%4 == 0:
+                self.image = self.bouliste[(self.spriteCount)]
+                self.spriteCount += 1
+                if self.spriteCount == 7:
+                    self.spriteCount =0
 
 
